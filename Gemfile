8cf358b4e06e6992092f904ca66957cd65ca9ef1
Patrick Lenz <patrick@limited-overload.de>
2013-10-07 16:44:32 +0200

    Disabled a couple plugins and added one for Autotask Todos

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 gem "activesupport"
 gem "rb-appscript"
-gem "highrise"
+gem "highrise", '~> 3.0'
 gem "zendesk_api"
 gem "autotask_api", github: 'scoop/autotask_api'
 gem "github_api"
---
+++ plugins/autotask_todos.rb
@@ -0,0 +1,74 @@
+#
+# Create OmniFocus tasks for tickets assigned to you in Autotask.
+# When tickets change in Autotask, those changes are reflected in OmniFocus. Changes
+# to tasks in OmniFocus are *not* synced back to Autotask at this point.
+#
+# Authentication data is taken from these environment variables:
+#
+#   AUTOTASK_WSDL: Contains the URL of the WSDL on the webservices host for your Autotask region
+#   AUTOTASK_USER: Contains the username (typically an email address) of your Autotask user
+#   AUTOTASK_PASS: Contains your Autotask password
+#
+# Additionally, the script needs the ID of the 'resolved' ticket status in Autotask
+# in the variable AUTOTASK_RESOLVED. Usually this is the ID 5. Lastly,
+# a comma-separated list of status IDs can be set in the variable
+# AUTOTASK_WAITING, listing all the IDs that map to your "Waiting For" context
+# in OmniFocus
+
+require "autotask_api"
+
+AUTOTASK_TODO_REGION = ENV['AUTOTASK_WSDL'].match(/webservices(\d)/)[1]
+
+@client = AutotaskAPI::Client.new do |client|
+    client.basic_auth = [
+      ENV['AUTOTASK_USER'],
+      ENV['AUTOTASK_PASS'],
+    ]
+    client.wsdl = ENV['AUTOTASK_WSDL']
+end
+
+@my_user = AutotaskAPI::Resource.find_by_email ENV['AUTOTASK_USER']
+
+@query = AutotaskAPI::QueryXML.new do |query|
+  query.entity = 'accounttodo'
+  query.field = 'AssignedToResourceID'
+  query.expression = @my_user.id
+end
+
+project = $omnifocus.flattened_projects["Autotask"].get
+
+def todo_name(todo)
+  account = AutotaskAPI::Account.find todo.account_id
+  "#{account.account_name}: #{todo.title} (##{todo.id})"
+end
+
+def todo_description(todo)
+  "#{todo.description}\n\nhttps://ww#{AUTOTASK_TODO_REGION}.autotask.net/Autotask/AutotaskExtend/ExecuteCommand.aspx?Code=OpenAccount&AccountID=#{todo.account_id}"
+end
+
+@client.entities_for(@query).each do |todo|
+  task = project.tasks[its.name.contains(todo.id)].first.get rescue nil
+
+  if task
+    unless todo.completed_date.blank?
+      unless task.completed.get
+        puts "Completing Todo #{todo.id} in OmniFocus"
+        task.completed.set true
+      end
+    else
+      update_if_changed task, :name, todo_name(todo)
+      update_if_changed task, :note, todo_description(todo)
+      update_if_changed task, :start_date, todo.start_time
+      update_if_changed task, :due_date, todo.end_time
+    end
+  else
+    puts "Adding Todo #{todo.id}"
+    project.make new: :task,
+      with_properties: {
+      name: todo_name(todo),
+      note: todo_description(todo),
+      start_date: todo.start_time,
+      due_date: todo.end_time
+    }
+  end
+end
